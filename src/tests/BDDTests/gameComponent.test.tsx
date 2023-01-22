3862c68b8462797d2f77216315a2db7deffc1428
Björn Andersson <bjorn.h.and@gmail.com>
2023-01-22 17:52:06 +0100

    bdd test testing a game round added

--- src/tests/BDDTests/gameComponent.test.tsx
+++ src/tests/BDDTests/gameComponent.test.tsx
@@ -1,11 +1,44 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
+import { pointsSystem as calculatePoints } from "../../config";
 
-const feature = loadFeature("../features/game.feature");
+const feature = loadFeature("./src/tests/features/game.feature");
 
 defineFeature(feature, (test) => {
-  test("playing through a game round", ({ given, when, then }) => {
-    given("category: History and difficulty: easy and region: GB", () => {});
-    when("correct answer is chosen", () => {});
-    then("Then points should be awarded", () => {});
+  let difficulty: string;
+  let difficultyPoints: number;
+  let correctAnswers: number;
+  let correctAnswersInARow: number;
+  let timeRemaining: number;
+  let points: number;
+
+  test("Playing through a game round with a correct answer", ({
+    given,
+    when,
+    then,
+  }) => {
+    given(
+      /^difficulty: medium and difficultyPoints: (\d+) and correctAnswers: (\d+) and correctAnswersInARow: (\d+) and timeRemaining: (\d+)$/,
+      (arg0, arg1, arg2, arg3) => {
+        difficulty = "medium";
+        difficultyPoints = parseInt(arg0);
+        correctAnswers = parseInt(arg1);
+        correctAnswersInARow = parseInt(arg2);
+        timeRemaining = parseInt(arg3);
+      }
+    );
+    when("player chooses correct answer", () => {
+      points = calculatePoints(
+        timeRemaining,
+        difficultyPoints,
+        correctAnswersInARow,
+        correctAnswers
+      );
+    });
+    then(
+      /^(\d+) points should be awarded if the answer is correct$/,
+      (expected) => {
+        expect(points).toBe(parseInt(expected));
+      }
+    );
   });
 });
--- src/tests/features/game.feature
+++ src/tests/features/game.feature
@@ -1,6 +1,6 @@
 Feature: GameComponent
 
-  Scenario
-    Given category: History and difficulty: easy and region: GB
-    When correct answer is chosen
-    Then points should be awarded
+  Scenario: Playing through a game round with a correct answer
+    Given difficulty: medium and difficultyPoints: 3 and correctAnswers: 4 and correctAnswersInARow: 4 and timeRemaining: 20
+    When player chooses correct answer
+    Then 76 points should be awarded if the answer is correct
